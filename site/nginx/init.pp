a5b06680ba7af0ba1455a55f232e95dbf107c86c
Robin Hart <robin_hart@hp.com>
2016-02-23 16:47:31 -0700

    homework11.2

--- site/nginx/init.pp
+++ site/nginx/init.pp
@@ -2,36 +2,42 @@ class nginx {
  
   package { 'nginx' :
     ensure => present,
+    before => [File['block'],File['config']],
   }
   
   file { 'docroot':
     ensure  => directory,
     path    => "/var/www",
-    require => Package['nginx'],
   }
   
-  file { 'index.html':
+  file { 'index':
     ensure  => file,
     path    => "/var/www/index.html",
     source  => 'puppet:///modules/nginx/index.html',
+    notify  => Service['nginx'],
   }
   
   file { 'config':
     ensure  => file,
     path    =>  "/etc/nginx/nginx.conf",
     source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
   
   file { 'block':
     ensure  => file,
     path    =>  "/etc/nginx/conf.d/default.conf",
     source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
   }
   
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => File['/var/www/index.html'],
+    require => [File['docroot'],File['index']],
+    subscribe => [File['config'],File['block']],
   }
   
 }
