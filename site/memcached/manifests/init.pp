a2484a54d3de5c775aa3713bdb5c08c3f5920e56
Robin Hart <robin_hart@hp.com>
2016-02-23 15:02:24 -0700

    Lab 11.1

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,18 @@
+class memcached {
+  
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure  => file,
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+  
+}
