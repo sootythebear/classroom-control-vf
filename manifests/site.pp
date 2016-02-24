59b50f2c094aa42ebfa154b38c84c0f53f53e89b
Robin Hart <robin_hart@hp.com>
2016-02-24 11:54:25 -0700

    exercise 13.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,15 +38,13 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-if ${::virtual} != 'physical' {
-  $hv = capitalize($::virtual)
-  notify { "My HV is: ${hv}\n": }
-}
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
-  
+  if ${::virtual} != 'physical' {
+    $hv = capitalize($::virtual)
+    notify { "My HV is: ${hv}\n": }
+  }
    #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
