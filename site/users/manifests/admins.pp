14348c94b032df25b98ad9ce4956d6d7ba9bf5cb
Robin Hart <robin_hart@hp.com>
2016-02-24 15:33:11 -0700

    Lab 15.1

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,10 @@
+class users::admins {
+
+  users::managed_users { 'robin' : 
+    $group => 'fundementals',
+  }
+  users::managed_users { 'jose' : }
+  users::managed_users { 'alice' : }
+  users::managed_users { 'chen' : }
+
+}
